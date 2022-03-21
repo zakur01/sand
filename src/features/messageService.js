1568b1bb3d017d7f03b691ab6ac5d3c130e5ec45
Ulanchik Moldousupovchik <sunnyhako324@gmail.com>
2022-03-21 23:48:49 +0600

    getMessages doesn't require a token

--- src/features/messageService.js
+++ src/features/messageService.js
@@ -3,13 +3,8 @@ import axios from 'axios';
 const API_URL_GET = 'https://strapi-sand.herokuapp.com/api/messages?populate=*';
 const API_URL = 'https://strapi-sand.herokuapp.com/api/messages/';
 
-const getMessages = async (token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-  return await axios.get(API_URL_GET, config);
+const getMessages = async () => {
+  return await axios.get(API_URL_GET);
 };
 
 const postMessage = async (payload, token) => {
